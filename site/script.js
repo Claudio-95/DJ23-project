f76aded5fd1ee0ebb389453556c1898fb60932cf
c.demartino <c.demartino1@studenti.unipi.it>
2023-07-14 18:16:12 +0200

    Add graph selector and some style changes

--- site/script.js
+++ site/script.js
@@ -17,11 +17,54 @@ function onClick(element) {
     captionText.innerHTML = element.alt;
 }
 
+// Ottenere i riferimenti ai pulsanti e al contenitore del grafico
+const graphicsContainer = document.getElementById('graphics-container');
+const graphics1Btn = document.getElementById('graphics1-btn');
+const graphics2Btn = document.getElementById('graphics2-btn');
+const graphics3Btn = document.getElementById('graphics3-btn');
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Funzioni di gestione degli eventi per i pulsanti
+    graphics1Btn.addEventListener('click', function() {
+        fetch('twitter_altair_charts/chartSentimentING.json')
+            .then(response => response.text())
+            .then(data => {
+                graphicsContainer.innerHTML = data;
+            })
+            .catch(error => {
+                console.error('Errore durante il caricamento del grafico 1:', error);
+            });
+    });
+
+    graphics2Btn.addEventListener('click', function() {
+        fetch('reddit_altair_charts/chartSentimentING_reddit.json')
+            .then(response => response.text())
+            .then(data => {
+                graphicsContainer.innerHTML = data;
+            })
+            .catch(error => {
+                console.error('Errore durante il caricamento del grafico 2:', error);
+            });
+    });
+
+    graphics3Btn.addEventListener('click', function() {
+        fetch('comparativeAnalysis_twitter_reddit.json')
+            .then(response => response.text())
+            .then(data => {
+                graphicsContainer.innerHTML = data;
+            })
+            .catch(error => {
+                console.error('Errore durante il caricamento del grafico 3:', error);
+            });
+    });
+});
+
+
 //bottone per visualizzazione dei grafici
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById('visualization-btn');
     const visualizations = document.getElementsByClassName('visualization');
-  
+
     button.addEventListener('click', function() {
       // Loop through all visualizations and toggle their active state
       for (let i = 0; i < visualizations.length; i++) {
@@ -29,4 +72,3 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
-  
