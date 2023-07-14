5f28a0468daba76a9b24ff49e68729d307f381f9
c.demartino <c.demartino1@studenti.unipi.it>
2023-07-14 19:28:48 +0200

    Fix graph selector and some updates

--- site/script.js
+++ site/script.js
@@ -18,57 +18,350 @@ function onClick(element) {
 }
 
 // Ottenere i riferimenti ai pulsanti e al contenitore del grafico
-const graphicsContainer = document.getElementById('graphics-container');
-const graphics1Btn = document.getElementById('graphics1-btn');
-const graphics2Btn = document.getElementById('graphics2-btn');
-const graphics3Btn = document.getElementById('graphics3-btn');
+const graphicsContainerSentiment = document.getElementById('graphics-container-sentiment');
+const graphics1BtnSentiment = document.getElementById('graphics1-btn-sentiment');
+const graphics2BtnSentiment = document.getElementById('graphics2-btn-sentiment');
+const graphics3BtnSentiment = document.getElementById('graphics3-btn-sentiment');
 
 document.addEventListener('DOMContentLoaded', function() {
     // Funzioni di gestione degli eventi per i pulsanti
-    graphics1Btn.addEventListener('click', function() {
-        fetch('twitter_altair_charts/chartSentimentING.json')
-            .then(response => response.text())
-            .then(data => {
-                graphicsContainer.innerHTML = data;
-            })
-            .catch(error => {
-                console.error('Errore durante il caricamento del grafico 1:', error);
-            });
+    graphics1BtnSentiment.addEventListener('click', function() {
+        // Crea il grafico Vega-Lite direttamente nel tuo script JavaScript
+        // chartSentimentING_reddit
+        const spec = {
+            "config": {
+            "view": {"continuousWidth": 300, "continuousHeight": 300},
+            "axis": {"grid": false},
+            "legend": {"orient": "top"}
+            },
+                "data": {"name": "data-800eee6c893eaac0f5534d5c99f91252"},
+                "mark": {"type": "area", "opacity": 0.1},
+                "encoding": {
+                "color": {
+                    "field": "sentiment_complete",
+                        "legend": {"orient": "right"},
+                    "scale": {
+                        "domain": ["Positivo", "Negativo", "Neutrale"],
+                            "range": ["#ADFC92", "#F44E3F", "#788BFF"]
+                    },
+                    "title": "Sentimento registrato",
+                        "type": "nominal"
+                },
+                "opacity": {"value": 0.9},
+                "tooltip": {"field": "sentiment_complete", "type": "nominal"},
+                "x": {
+                    "axis": {"format": "%B %Y", "tickCount": 12, "title": "Periodo"},
+                    "field": "Comment date",
+                        "type": "temporal"
+                },
+                "y": {
+                    "axis": {"title": "Numero di commenti"},
+                    "field": "Value",
+                        "type": "quantitative"
+                }
+            },
+                "height": 600,
+                "params": [
+                {
+                    "name": "param_28",
+                    "select": {"type": "interval", "encodings": ["x", "y"]},
+                    "bind": "scales"
+                }
+            ],
+                "title": {
+                "text": "Evoluzione del sentimento verso Chat-GPT nell'arco dei mesi",
+                    "subtitle": "Visualizzazione dei valori relativi al dataset di Commenti"
+            },
+                "width": 800,
+                "$schema": "https://vega.github.io/schema/vega-lite/v5.8.0.json",
+                "datasets": {
+                "data-800eee6c893eaac0f5534d5c99f91252": [
+                    {
+                        "Comment date": "April 2023",
+                        "sentiment_complete": "Negativo",
+                        "Value": 4717
+                    },
+                    {
+                        "Comment date": "May 2023",
+                        "sentiment_complete": "Negativo",
+                        "Value": 9471
+                    },
+                    {
+                        "Comment date": "June 2023",
+                        "sentiment_complete": "Negativo",
+                        "Value": 2339
+                    },
+                    {
+                        "Comment date": "April 2023",
+                        "sentiment_complete": "Neutrale",
+                        "Value": 5138
+                    },
+                    {
+                        "Comment date": "May 2023",
+                        "sentiment_complete": "Neutrale",
+                        "Value": 10454
+                    },
+                    {
+                        "Comment date": "June 2023",
+                        "sentiment_complete": "Neutrale",
+                        "Value": 3002
+                    },
+                    {
+                        "Comment date": "April 2023",
+                        "sentiment_complete": "Positivo",
+                        "Value": 9626
+                    },
+                    {
+                        "Comment date": "May 2023",
+                        "sentiment_complete": "Positivo",
+                        "Value": 18334
+                    },
+                    {
+                        "Comment date": "June 2023",
+                        "sentiment_complete": "Positivo",
+                        "Value": 4897
+                    }
+                ]
+            }
+        };
+
+        // Rendi il grafico visibile in un contenitore HTML
+        vegaEmbed('#graphics-container-sentiment', spec);
     });
 
-    graphics2Btn.addEventListener('click', function() {
-        fetch('reddit_altair_charts/chartSentimentING_reddit.json')
-            .then(response => response.text())
-            .then(data => {
-                graphicsContainer.innerHTML = data;
-            })
-            .catch(error => {
-                console.error('Errore durante il caricamento del grafico 2:', error);
-            });
+    graphics2BtnSentiment.addEventListener('click', function() {
+        // chartITsentiment
+        const spec = {
+            "config": {"view": {"continuousWidth": 300, "continuousHeight": 300}},
+            "data": {"name": "data-f5aa40d221b8f095fd9ec2a8cf953d4d"},
+            "mark": {"type": "line", "opacity": 0.1},
+            "encoding": {
+                "color": {
+                    "field": "sentiment_complete",
+                        "legend": {"orient": "right"},
+                    "scale": {
+                        "domain": ["Positivo", "Neutrale", "Negativo"],
+                            "range": ["#ADFC92", "#788BFF", "#F44E3F"]
+                    },
+                    "title": "Sentimento registrato",
+                        "type": "nominal"
+                },
+                "opacity": {"value": 0.9},
+                "x": {
+                    "axis": {"format": "%B %Y", "tickCount": 12, "title": "Periodo"},
+                    "field": "date",
+                        "type": "temporal"
+                },
+                "y": {
+                    "axis": {"title": "Numero di tweets"},
+                    "field": "Value",
+                        "type": "quantitative"
+                }
+            },
+                "height": 600,
+                "title": {
+                "text": "Evoluzione del sentimento verso Chat GPT nell'arco dei mesi",
+                    "subtitle": "Visualizzazione dei valori relativi al dataset di Tweets"
+            },
+                "width": 800,
+                "$schema": "https://vega.github.io/schema/vega-lite/v5.8.0.json",
+                "datasets": {
+                "data-f5aa40d221b8f095fd9ec2a8cf953d4d": [
+                    {"date": "September 2022", "sentiment_complete": "Negativo", "Value": 64},
+                    {"date": "October 2022", "sentiment_complete": "Negativo", "Value": 103},
+                    {
+                        "date": "December 2022",
+                        "sentiment_complete": "Negativo",
+                        "Value": 12890
+                    },
+                    {
+                        "date": "January 2023",
+                        "sentiment_complete": "Negativo",
+                        "Value": 21900
+                    },
+                    {
+                        "date": "February 2023",
+                        "sentiment_complete": "Negativo",
+                        "Value": 6205
+                    },
+                    {"date": "March 2023", "sentiment_complete": "Negativo", "Value": 3109},
+                    {"date": "April 2023", "sentiment_complete": "Negativo", "Value": 2138},
+                    {"date": "May 2023", "sentiment_complete": "Negativo", "Value": 1901},
+                    {"date": "June 2023", "sentiment_complete": "Negativo", "Value": 999},
+                    {
+                        "date": "September 2022",
+                        "sentiment_complete": "Neutrale",
+                        "Value": 101
+                    },
+                    {"date": "October 2022", "sentiment_complete": "Neutrale", "Value": 75},
+                    {
+                        "date": "December 2022",
+                        "sentiment_complete": "Neutrale",
+                        "Value": 26609
+                    },
+                    {
+                        "date": "January 2023",
+                        "sentiment_complete": "Neutrale",
+                        "Value": 44188
+                    },
+                    {
+                        "date": "February 2023",
+                        "sentiment_complete": "Neutrale",
+                        "Value": 11888
+                    },
+                    {"date": "March 2023", "sentiment_complete": "Neutrale", "Value": 4615},
+                    {"date": "April 2023", "sentiment_complete": "Neutrale", "Value": 3418},
+                    {"date": "May 2023", "sentiment_complete": "Neutrale", "Value": 3131},
+                    {"date": "June 2023", "sentiment_complete": "Neutrale", "Value": 1545},
+                    {
+                        "date": "September 2022",
+                        "sentiment_complete": "Positivo",
+                        "Value": 114
+                    },
+                    {"date": "October 2022", "sentiment_complete": "Positivo", "Value": 104},
+                    {
+                        "date": "December 2022",
+                        "sentiment_complete": "Positivo",
+                        "Value": 37278
+                    },
+                    {
+                        "date": "January 2023",
+                        "sentiment_complete": "Positivo",
+                        "Value": 67973
+                    },
+                    {
+                        "date": "February 2023",
+                        "sentiment_complete": "Positivo",
+                        "Value": 17068
+                    },
+                    {"date": "March 2023", "sentiment_complete": "Positivo", "Value": 7458},
+                    {"date": "April 2023", "sentiment_complete": "Positivo", "Value": 5070},
+                    {"date": "May 2023", "sentiment_complete": "Positivo", "Value": 4859},
+                    {"date": "June 2023", "sentiment_complete": "Positivo", "Value": 2506}
+                ]
+            }
+        };
+
+        vegaEmbed('#graphics-container-sentiment', spec);
     });
 
-    graphics3Btn.addEventListener('click', function() {
-        fetch('comparativeAnalysis_twitter_reddit.json')
-            .then(response => response.text())
-            .then(data => {
-                graphicsContainer.innerHTML = data;
-            })
-            .catch(error => {
-                console.error('Errore durante il caricamento del grafico 3:', error);
-            });
+    graphics3BtnSentiment.addEventListener('click', function() {
+        // chartSentimentING
+        const spec = {
+            "config": {
+            "view": {"continuousWidth": 300, "continuousHeight": 300},
+            "axis": {"grid": false},
+            "legend": {"orient": "top"}
+            },
+                "data": {"name": "data-f5aa40d221b8f095fd9ec2a8cf953d4d"},
+                "mark": {"type": "line", "opacity": 0.1},
+                "encoding": {
+                "color": {
+                    "field": "sentiment_complete",
+                        "legend": {"orient": "right"},
+                    "scale": {
+                        "domain": ["Positivo", "Neutrale", "Negativo"],
+                            "range": ["#ADFC92", "#788BFF", "#F44E3F"]
+                    },
+                    "title": "Sentimento registrato",
+                        "type": "nominal"
+                },
+                "opacity": {"value": 0.9},
+                "x": {
+                    "axis": {"format": "%B %Y", "tickCount": 12, "title": "Periodo"},
+                    "field": "date",
+                        "type": "temporal"
+                },
+                "y": {
+                    "axis": {"title": "Numero di tweets"},
+                    "field": "Value",
+                        "type": "quantitative"
+                }
+            },
+                "height": 600,
+                "title": {
+                "text": "Evoluzione del sentimento verso Chat GPT nell'arco dei mesi",
+                    "subtitle": "Visualizzazione dei valori relativi al dataset di Tweets"
+            },
+                "width": 800,
+                "$schema": "https://vega.github.io/schema/vega-lite/v5.8.0.json",
+                "datasets": {
+                "data-f5aa40d221b8f095fd9ec2a8cf953d4d": [
+                    {"date": "September 2022", "sentiment_complete": "Negativo", "Value": 64},
+                    {"date": "October 2022", "sentiment_complete": "Negativo", "Value": 103},
+                    {
+                        "date": "December 2022",
+                        "sentiment_complete": "Negativo",
+                        "Value": 12890
+                    },
+                    {
+                        "date": "January 2023",
+                        "sentiment_complete": "Negativo",
+                        "Value": 21900
+                    },
+                    {
+                        "date": "February 2023",
+                        "sentiment_complete": "Negativo",
+                        "Value": 6205
+                    },
+                    {"date": "March 2023", "sentiment_complete": "Negativo", "Value": 3109},
+                    {"date": "April 2023", "sentiment_complete": "Negativo", "Value": 2138},
+                    {"date": "May 2023", "sentiment_complete": "Negativo", "Value": 1901},
+                    {"date": "June 2023", "sentiment_complete": "Negativo", "Value": 999},
+                    {
+                        "date": "September 2022",
+                        "sentiment_complete": "Neutrale",
+                        "Value": 101
+                    },
+                    {"date": "October 2022", "sentiment_complete": "Neutrale", "Value": 75},
+                    {
+                        "date": "December 2022",
+                        "sentiment_complete": "Neutrale",
+                        "Value": 26609
+                    },
+                    {
+                        "date": "January 2023",
+                        "sentiment_complete": "Neutrale",
+                        "Value": 44188
+                    },
+                    {
+                        "date": "February 2023",
+                        "sentiment_complete": "Neutrale",
+                        "Value": 11888
+                    },
+                    {"date": "March 2023", "sentiment_complete": "Neutrale", "Value": 4615},
+                    {"date": "April 2023", "sentiment_complete": "Neutrale", "Value": 3418},
+                    {"date": "May 2023", "sentiment_complete": "Neutrale", "Value": 3131},
+                    {"date": "June 2023", "sentiment_complete": "Neutrale", "Value": 1545},
+                    {
+                        "date": "September 2022",
+                        "sentiment_complete": "Positivo",
+                        "Value": 114
+                    },
+                    {"date": "October 2022", "sentiment_complete": "Positivo", "Value": 104},
+                    {
+                        "date": "December 2022",
+                        "sentiment_complete": "Positivo",
+                        "Value": 37278
+                    },
+                    {
+                        "date": "January 2023",
+                        "sentiment_complete": "Positivo",
+                        "Value": 67973
+                    },
+                    {
+                        "date": "February 2023",
+                        "sentiment_complete": "Positivo",
+                        "Value": 17068
+                    },
+                    {"date": "March 2023", "sentiment_complete": "Positivo", "Value": 7458},
+                    {"date": "April 2023", "sentiment_complete": "Positivo", "Value": 5070},
+                    {"date": "May 2023", "sentiment_complete": "Positivo", "Value": 4859},
+                    {"date": "June 2023", "sentiment_complete": "Positivo", "Value": 2506}
+                ]
+            }
+        };
+
+        vegaEmbed('#graphics-container-sentiment', spec);
     });
 });
-
-
-//bottone per visualizzazione dei grafici
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.getElementById('visualization-btn');
-    const visualizations = document.getElementsByClassName('visualization');
-
-    button.addEventListener('click', function() {
-      // Loop through all visualizations and toggle their active state
-      for (let i = 0; i < visualizations.length; i++) {
-        visualizations[i].classList.toggle('active');
-      }
-    });
-  });
